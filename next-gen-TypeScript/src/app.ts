8156086f2b43d1fdd6bab46de7e6dd527043c570
dvp-tae <th3016@naver.com>
2024-04-18 15:41:34 +0900

    240418

--- next-gen-TypeScript/src/app.ts
+++ next-gen-TypeScript/src/app.ts
@@ -17,24 +17,24 @@
 
 // printOut(add(4));
 
-const hobbies = ['Sports', 'Cooking', 'Driving', 'Movie'];
-const activeHobbies = ['Hiking'];
+const hobbies = ["Sports", "Cooking", "Driving", "Movie"];
+const activeHobbies = ["Hiking"];
 
 activeHobbies.push(...hobbies);
 
-// console.log(activeHobbies);
+console.log(activeHobbies);
 
 const person = {
-  firstName: 'Max',
-  age: 25
-}
+  firstName: "Max",
+  age: 25,
+};
 
 const newPerson = { ...person };
 
 const add = (...numbers: number[]) => {
-  return numbers.reduce((curResult, curValue)=>{
+  return numbers.reduce((curResult, curValue) => {
     return curResult + curValue;
-  },0);
+  }, 0);
 };
 const addedNumbers = add(5, 10, 2, 3.7);
 // console.log(addedNumbers);
