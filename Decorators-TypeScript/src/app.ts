085216b4474afbdb6e226207455ef785180d7d6b
dvp-tae <th3016@naver.com>
2024-04-22 13:13:55 +0900

    240422

--- Decorators-TypeScript/src/app.ts
+++ Decorators-TypeScript/src/app.ts
@@ -1,46 +1,50 @@
 /* 데코레이터 생성 방법 (@Logger 를 이용해 사용) */
 // function Logger(constructor: Function) {
-//   console.log('Logging...');
-//   console.log(constructor); 
+//   console.log("Logging...");
+//   console.log(constructor);
 // }
 
 /* 데코레이터 팩토리 정의 (@Logger() 를 이용해 사용)*/
 function Logger(logString: string) {
-  console.log("LOGGER FACTORY");         // 실행 순서1
-  return function(constructor: Function) {
+  console.log("LOGGER FACTORY"); // 실행 순서1
+  return function (constructor: Function) {
     console.log(logString);
-    console.log(constructor)
-  }
+    console.log(constructor);
+  };
 }
 
 /* 데코레이터 함수의 생성 순서는 팩토리 함수를 정의한 순서대로 생성 */
 /* 데코레이터 함수 실행 순서는 상향식(bottom-up)으로 진행 */
 
 function WithTemplate(template: string, hookId: string) {
-  console.log("TEMPLATE FACTORY");       //실행 순서2
-  return function<T extends {new(...args: any[]): {name: string}}>(originalConstructor: T) {  
-    return class extends originalConstructor {   // 생성자 함수를 만들어 새 로직을 추가하기 위해 클래스를 만듦
-      constructor(..._: any[]) {                 // _ 연산자 => 타입스크립트에게 인자가 들어오는 건 알리지만 필요하지 않음을 표시하는 연산자
+  console.log("TEMPLATE FACTORY"); //실행 순서2
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor {
+      // 생성자 함수를 만들어 새 로직을 추가하기 위해 클래스를 만듦
+      constructor(..._: any[]) {
+        // _ 연산자 => 타입스크립트에게 인자가 들어오는 건 알리지만 필요하지 않음을 표시하는 연산자
         super();
-        console.log('Rendering template');
+        console.log("Rendering template");
         const hookEl = document.getElementById(hookId);
         if (hookEl) {
           hookEl.innerHTML = template;
-          hookEl.querySelector('h1')!.textContent = this.name;
+          hookEl.querySelector("h1")!.textContent = this.name;
         }
       }
-    }
-  }
+    };
+  };
 }
 
 // @Logger('LOGGING - PERSON')
-@Logger('LOGGING')
-@WithTemplate('<h1>My Person Object</h1>','app')
+@Logger("LOGGING")
+@WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
-  name = 'Max';
+  name = "Max";
 
   constructor() {
-    console.log('Creating person object...');
+    console.log("Creating person object...");
   }
 }
 
@@ -53,12 +57,12 @@ console.log(pers);
 // 프로퍼티에 데코레이터를 추가하면 2개의 인자가 들어옴
 // 첫 번재 인자
 // 인스턴스 프로퍼티 -> 타깃 인자로 생성된 객체의 프로토타입
-// 정적 프로퍼티 -> 생성자 
+// 정적 프로퍼티 -> 생성자
 // 두 번째 인자
 // 프로퍼티 이름
 
 function Log(target: any, propertyName: string | Symbol) {
-  console.log('Property decorator!');
+  console.log("Property decorator!");
   console.log(target);
   console.log(propertyName);
 }
@@ -66,7 +70,7 @@ function Log(target: any, propertyName: string | Symbol) {
 // 접근자에 데코레이터 추가
 
 function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
-  console.log('Accessor decorator!');
+  console.log("Accessor decorator!");
   console.log(target);
   console.log(name);
   console.log(descriptor);
@@ -74,8 +78,12 @@ function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
 
 // 메서드에 데코레이터 추가
 
-function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
-  console.log('Method decorator!');
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method decorator!");
   console.log(target);
   console.log(name);
   console.log(descriptor);
@@ -83,34 +91,36 @@ function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor
 
 // 파라미터에 데코레이터 추가
 
-function Log4(target: any, name: string | Symbol, position: number) {   // 파라미터에 데코레이터 추가할 때는 3번재 인자로 메소드 내 해당 파라미터 위치 정보 넣어줌
-  console.log('Parameter decorator!');
+function Log4(target: any, name: string | Symbol, position: number) {
+  // 파라미터에 데코레이터 추가할 때는 3번재 인자로 메소드 내 해당 파라미터 위치 정보 넣어줌
+  console.log("Parameter decorator!");
   console.log(target);
   console.log(name);
   console.log(position);
 }
 
 class Product {
-  @Log  // 프로퍼티에 추가되는 데코레이터
+  @Log // 프로퍼티에 추가되는 데코레이터
   title: string;
   private _price: number;
 
-  @Log2  // 접근자에 추가되는 데코레이터 (접근자 -> getter setter)
+  @Log2 // 접근자에 추가되는 데코레이터 (접근자 -> getter setter)
   set price(val: number) {
     if (val > 0) {
       this._price = val;
     } else {
-      throw new Error('Invalid price = should be positive!');
+      throw new Error("Invalid price = should be positive!");
     }
   }
 
-  constructor(t: string, p:number) {
+  constructor(t: string, p: number) {
     this.title = t;
     this._price = p;
   }
 
-  @Log3  // 메서드에 추가되는 데코레이터
-  getPriceWithTax(@Log4 tax: number) {   // 매개변수에 추가되는 데코레이터
+  @Log3 // 메서드에 추가되는 데코레이터
+  getPriceWithTax(@Log4 tax: number) {
+    // 매개변수에 추가되는 데코레이터
     return this._price * (1 + tax);
   }
 }
@@ -118,22 +128,23 @@ class Product {
 // const p1 = new Product('Book', 19);
 // const p2 = new Product('Book 2', 29);
 
-
-function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {    // addEventListener 추가 시 this가 가리키는 것을 원본 함수로 bind하기 위한 데코레이터
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  // addEventListener 추가 시 this가 가리키는 것을 원본 함수로 bind하기 위한 데코레이터
   const originalMethod = descriptor.value;
   const adjDescriptor: PropertyDescriptor = {
     configurable: true,
     enumerable: false,
-    get() {  // 게터는 value 프로퍼티에 값을 반환하기 전에 실행할 부가 로직이 합쳐진 형태
+    get() {
+      // 게터는 value 프로퍼티에 값을 반환하기 전에 실행할 부가 로직이 합쳐진 형태
       const boundFn = originalMethod.bind(this);
       return boundFn;
-    }
+    },
   };
   return adjDescriptor;
 }
 
 class Printer {
-  mesage = 'This works!';
+  mesage = "This works!";
 
   @AutoBind
   showMessage() {
@@ -144,17 +155,15 @@ class Printer {
 const p = new Printer();
 p.showMessage();
 
-const button = document.querySelector('button')!;
-button.addEventListener('click',p.showMessage);
-
-
+const button = document.querySelector("button")!;
+button.addEventListener("click", p.showMessage);
 
 // ---
 
 interface ValidatorConfig {
   [property: string]: {
-    [validatableProp: string]: string[] // ['required', 'positive']
-  }
+    [validatableProp: string]: string[]; // ['required', 'positive']
+  };
 }
 
 const registerValidators: ValidatorConfig = {};
@@ -162,14 +171,21 @@ const registerValidators: ValidatorConfig = {};
 function Required(target: any, propName: string) {
   registerValidators[target.constructor.name] = {
     ...registerValidators[target.constructor.name],
-    [propName]: [...(registerValidators[target.constructor.name]?.[propName] ?? []), 'required']
+    [propName]: [
+      ...(registerValidators[target.constructor.name]?.[propName] ?? []),
+      "required",
+    ],
   };
 }
 
 function PositiveNumber(target: any, propName: string) {
   registerValidators[target.constructor.name] = {
     ...registerValidators[target.constructor.name],
-    [propName]: [...(registerValidators[target.constructor.name]?.[propName] ?? []), 'positive','limit']
+    [propName]: [
+      ...(registerValidators[target.constructor.name]?.[propName] ?? []),
+      "positive",
+      "limit",
+    ],
   };
 }
 
@@ -182,13 +198,13 @@ function validate(obj: any) {
   for (const prop in objValidatorConfig) {
     for (const validator of objValidatorConfig[prop]) {
       switch (validator) {
-        case 'required':
+        case "required":
           isValid = isValid && !!obj[prop];
           break;
-        case 'positive':
+        case "positive":
           isValid = isValid && obj[prop] > 0;
           break;
-        case 'limit':
+        case "limit":
           isValid = isValid && obj[prop] < 1000;
           break;
       }
@@ -203,17 +219,17 @@ class Course {
   @PositiveNumber
   price: number;
 
-  constructor(t: string, p:number) {
+  constructor(t: string, p: number) {
     this.title = t;
     this.price = p;
   }
 }
 
-const courseForm = document.querySelector('form')!;
-courseForm.addEventListener('submit', event => {
+const courseForm = document.querySelector("form")!;
+courseForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  const titleEl = document.getElementById('title') as HTMLInputElement;
-  const priceEl = document.getElementById('price') as HTMLInputElement;
+  const titleEl = document.getElementById("title") as HTMLInputElement;
+  const priceEl = document.getElementById("price") as HTMLInputElement;
 
   const title = titleEl.value;
   const price = +priceEl.value;
@@ -221,9 +237,9 @@ courseForm.addEventListener('submit', event => {
   const createdCourse = new Course(title, price);
 
   if (!validate(createdCourse)) {
-    alert('Invalid input, please try again!');
+    alert("Invalid input, please try again!");
     return;
   }
 
   console.log(createdCourse);
-})
+});
